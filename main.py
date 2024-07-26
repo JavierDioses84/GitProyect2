4ca75e9f0826b8b364955a0279b8d084b8e44cff
JavierDioses84 <javier.diosesz@gmail.com>
2024-07-26 15:50:53 -0500

    Actualizando cambios en main y exploracionDx

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+#Si se muestra el error: Missing optional dependency 'openpyxl'
+# import pip
+# pip.main(["install", "openpyxl"])
+
+import exploracionDx as e1
+
+data = pd.read_excel('Inmuebles.xlsx')
+print(data.head(10))
+
+print(e1.correlacionPares())
